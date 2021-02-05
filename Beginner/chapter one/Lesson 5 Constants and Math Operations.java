727ae6166c65b57c1f071ecd6d5f6109a76a7672
Timz Owen <ktimothy@kabarak.ac.ke>
2021-02-05 10:24:35 +0300

    Update Lesson 5 Constants and Math Operations.java
    type conversions

--- Beginner/chapter one/Lesson 5 Constants and Math Operations.java
+++ Beginner/chapter one/Lesson 5 Constants and Math Operations.java
@@ -28,3 +28,49 @@
     System.out.println(remainder);
     System.out.println(intrest);
     System.out.println(area);
+
+      // Values increment and decrement
+    int x = 2;
+    int y = ++x;
+    int z = x++;
+    System.out.println(y);
+    System.out.println(z);
+
+    // augmented operators (+=, /=, *=)
+    int i = 2;
+    i = i + 2;
+    System.out.println(i);
+    i += 2;
+    System.out.println(i);
+
+    
+    // flow of execution:
+    // BODMAS
+    int x = 10 + 5 * 8;
+    int j = (10 + 5) * 8;
+
+    System.out.println(x);
+    System.out.println(j);
+
+    // TYPE CONVERSION & CASTING:
+    // conversion happend from: byte>short>int>long>float>double
+    int a = 20;  // 4 bytes
+    short b = 5; // 2 bytes
+    System.out.println(a + b); // IDE does implicit conversion to one of the type and performs the operation
+
+
+    // Explicit conversion:
+    int a = 5;
+    double b = 5.5;
+    int results = (int)b + a;  //casting a double to an integer
+    System.out.println(results);
+
+
+    //Strings and integers:
+    String myAge = "23";
+    int weekDays = 7;
+    String weight = "70.50";
+    double weightSum = Double.parseDouble(weight) + Integer.parseInt(myAge);
+    System.out.println(weightSum);
+    int total_sum = Integer.parseInt(myAge) + weekDays;
+    System.out.println(total_sum);
