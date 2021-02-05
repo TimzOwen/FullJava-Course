d7a0d16d2c96a6a267cf1374aff95c62be71bd09
Timz Owen <ktimothy@kabarak.ac.ke>
2021-02-05 10:20:55 +0300

    Update Lesson 3 Arrays.java
    arrays and 2-D arrays

--- Beginner/chapter one/Lesson 3 Arrays.java
+++ Beginner/chapter one/Lesson 3 Arrays.java
@@ -1,1 +1,37 @@
 
+   // ARRAYS:
+    // used to store list of items
+    // "new" keyword is used because they are refence type
+    int[] arrayNum = new int[5];
+    arrayNum[0] = 2;
+    arrayNum[1] = 4;
+    arrayNum[2] = 6;
+    arrayNum[3] = 8;
+    //to print all array you have to convert them to strings, otherwise prints
+    address index:
+    System.out.println(arrayNum);
+    System.out.println(Arrays.toString(arrayNum)); // this imports the array
+    // also N/B unassigned int values set to 0, boolean= false, string = " "
+
+    // Easy way is to juse curl brackets:
+    int[] numbers = {2,4,6,8,10,1,3,5,7,12,20};
+    System.out.println(Arrays.toString(numbers));
+    System.out.println(numbers.length); // Returns the total number of elements;
+    Arrays.sort(numbers); // sorts values in an array:
+    System.out.println(Arrays.toString(numbers)); // sorts values in ascending
+    order for integers;
+
+    // MULTI-DIMENTIONAL ARRAYS:
+    // working with 2 dimentional array
+    //to print use "deepToString" and not toString
+    int[][] number2D = new int[2][3];
+    number2D[0][0] = 2;
+    number2D[1][0] = 4;
+    number2D[1][1] = 6;
+    number2D[0][2] = 8;
+
+    System.out.println(Arrays.deepToString(number2D));
+
+    // 2-D arrays in Culr brackets:
+    int[][] twoDArray = {{2,4,6},{8,10,12}};
+    System.out.println(Arrays.deepToString(twoDArray));
