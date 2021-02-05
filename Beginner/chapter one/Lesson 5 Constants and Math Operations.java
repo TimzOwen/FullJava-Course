3c0befe2c176c754006b5ddf5764ca45cf3defe6
Timz Owen <ktimothy@kabarak.ac.ke>
2021-02-05 10:23:08 +0300

    Update Lesson 5 Constants and Math Operations.java
    java constants and basic operation

--- Beginner/chapter one/Lesson 5 Constants and Math Operations.java
+++ Beginner/chapter one/Lesson 5 Constants and Math Operations.java
@@ -1,1 +1,30 @@
 
+ 
+    // JAVA CONSTANTS:
+    // used when we have values that need to remain the same all throughout the
+    program
+    // "final" keyword is used before any data type
+    final float transactionRate = 5.25F;
+    final float areaCircle = 3.412F;
+    // values for this case cannot be reassigned here.
+    System.out.println(transactionRate);
+    System.out.println(areaCircle);
+
+    // MATHS IN JAVA
+    // addition,sub,div,multiplication;
+    double i = 20;
+    double j = 10.5;
+
+    int addition = 20 + 10;
+    int subtraction = 20 - 10;
+    int division = 9 / 3;
+    double intrest = (double)10 / (double)3; // casting to allow double values
+    int remainder = 10 % 2;
+    double area = j * i;
+
+    System.out.println(addition);
+    System.out.println(subtraction);
+    System.out.println(division);
+    System.out.println(remainder);
+    System.out.println(intrest);
+    System.out.println(area);
