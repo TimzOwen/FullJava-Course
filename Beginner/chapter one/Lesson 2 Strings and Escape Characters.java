e8278f510e83a32aecc1d32c92384f49fa66d489
Timz Owen <ktimothy@kabarak.ac.ke>
2021-02-05 10:18:15 +0300

    Update Lesson 2 Strings and Escape Characters.java
    strings update

--- Beginner/chapter one/Lesson 2 Strings and Escape Characters.java
+++ Beginner/chapter one/Lesson 2 Strings and Escape Characters.java
@@ -1,1 +1,40 @@
 
+   // STRINGS:
+    // Removing string from their literal and placing them in values
+    // They are of reference type and no need of 'new' key words
+    System.out.println("Hello World !");
+    String myName = " timz owen ";
+    System.out.println(myName);
+
+    // String concatenation
+    String mySchool = "Kabarak " + "University";
+    System.out.println(mySchool);
+
+    // Strings with methods:
+    boolean booleanEndsWith = mySchool.endsWith("ty"); //endsWith method
+    System.out.println(booleanEndsWith);
+    System.out.println(mySchool.startsWith("mk")); // returns False
+    System.out.println(myName.length()); // cal lenght of string characters (n/b:
+    spaces counted)
+    System.out.println(mySchool.indexOf("a")); // returns index of first char
+    occurance
+    System.out.println(myName.replace("o", "O")); // takes in a target and
+    replpacement values
+
+    String shoutHey = "Hey !!! ";
+    System.out.println(shoutHey.replace("!", "@")); // saves the value to new
+    variable. not modify 1st one
+    System.out.println(shoutHey); // returns the original value without
+    modification
+
+    System.out.println(myName.toLowerCase());
+    System.out.println(myName.toUpperCase());
+    System.out.println(myName.trim()); // remove unnecessary white spaces
+
+    // ESCAPING CHARACCTERS IN JAVA
+    String doubleQuote = "I am the \"local man\" coding java";
+    System.out.println(doubleQuote);
+    String driveLocation = "\\usr\\bin\\local...";
+    System.out.println(driveLocation);
+    System.out.println("welcome \n to Java \n python");
+    System.out.println("I love\t using\t tabs\n");
