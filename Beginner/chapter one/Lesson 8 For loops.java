e61a32f1e4d76f29a61bda804f63c93e76c0ddec
Timz Owen <ktimothy@kabarak.ac.ke>
2021-02-27 10:07:03 +0300

    Update and rename Lesson 8 While loops.java to Lesson 8 For loops.java
    for loop update

---
+++ Beginner/chapter one/Lesson 8 For loops.java
@@ -0,0 +1,21 @@
+
+   // FOR LOOPS:
+
+    // used for executing repetitive tasks
+    // declare counter loop varibale(initialization), how many times to be executed, increment/decrement
+
+    // print with parenthesis
+    System.out.println("hello world 5 times");
+    System.out.println("hello world 5 times");
+    System.out.println("hello world 5 times");
+    System.out.println("hello world 5 times");
+    System.out.println("hello world 5 times");
+
+    // looping incrementing 
+    for (int a = 1; a<5; a++)
+      System.out.println("Loop at number: " + a);
+
+    // decrementing
+    for (int i = 5; i>0; i--){
+      System.out.println("decrementing values, current number: " + i);
+    }
