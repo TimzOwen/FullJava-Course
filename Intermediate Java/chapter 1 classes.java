ebbae2f023a761233d6c38db4058606cdf5daf17
Timz Owen <ktimothy@kabarak.ac.ke>
2021-03-24 13:10:55 +0300

    Update chapter 1 classes.java
    class architecture

--- Intermediate Java/chapter 1 classes.java
+++ Intermediate Java/chapter 1 classes.java
@@ -1,1 +1,27 @@
 
+
+// classes and Objects
+// objects and blueprint
+
+// Building and architecture is the blue-print of the final desired building or
+// object----> blueprint(=class)
+// every object has behaviour and values
+
+// architecture
+
+class Calculator{
+
+    // behavior and attribute of the class here
+
+}
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        // call the function here after creating an object instance of it.
+        System.out.println(" hello java classes ");
+
+        Calculator calc = new Calculator(); // creating an object
+        // calc.attribute/value
+    }
+}
