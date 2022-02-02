744f5e4c095fe5eba72e61675e13f61a5bbd90eb
TimzOwen <ktimothy.com>
2022-02-02 21:13:14 +0300

    updated practice excercise

--- Java Practice Questions/Beginner Track/chapter_one.java
+++ Java Practice Questions/Beginner Track/chapter_one.java
@@ -1,8 +1,6 @@
 // // probelem statement
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-
-
 public int[] twoSum(int[] nums, int target) {
     Map<Integer, Integer> map = new HashMap<>();
     for (int i = 0; i < nums.length; i++) {
