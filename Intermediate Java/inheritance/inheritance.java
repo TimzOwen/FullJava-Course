c9b0e14972a7c52e82f91935c8156a9c501dc734
TimzOwen <ktimothy@kabarak.ac.ke>
2022-03-18 22:26:13 +0300

    explanation

---
+++ Intermediate Java/inheritance/inheritance.java
@@ -0,0 +1,24 @@
+// polymorphism in one language
+
+// to draw shapes without instantiating new obect instance 
+// The shape uses the Shape object which is inherited by Circle and Traingle to draw irespective shapes
+//without creating new instances on new obects
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Shape shape = new Shape();
+        shape.draw();
+
+        Circle circle = new Circle();
+        draw()
+
+        Triangle triangle = new Triangle();
+        draw();
+
+	
+    }
+}
+
+
