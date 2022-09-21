293852f1628f738f4e45b9a3e7306fe7ef3942e5
TimzOwen <timzowen@gmail.com>
2022-09-21 22:20:00 +0300

    .

--- Java Practice Questions/Beginner Track/leetcode.java
+++ Java Practice Questions/Beginner Track/leetcode.java
@@ -2,9 +2,7 @@ public class leetcode {
     public static void main(String args[]){
 
         System.out.println("Testing leet code ");
-
-        int a = 40;
-        System.out.println(a);
+        System.out.println("testing");
 
     }
 }
--- Java Practice Questions/Beginner Track/regex.java
+++
@@ -1,2 +0,0 @@
-// working with validation using Java Regex.
-
