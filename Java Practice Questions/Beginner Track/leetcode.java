5e69672df01c143b2b06f640557d197f9207d33e
TimzOwen <timzowen@gmail.com>
2022-09-23 17:30:13 +0300

    string and inputs

--- Java Practice Questions/Beginner Track/leetcode.java
+++ Java Practice Questions/Beginner Track/leetcode.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
+
 public class leetcode {
-    public static void main(String args[]){
+    public static void main(String[] args) {
+        
+        // 1. inouts
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d  = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        scan.close();
+        
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
 
-        System.out.println("Testing leet code ");
-        System.out.println("testing");
 
+
+        // 2. string format
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            System.out.println(String.format("%-15s%03d",s1,x));
+        }
+        System.out.println("================================");
+
+        // 3. 
     }
+
 }
