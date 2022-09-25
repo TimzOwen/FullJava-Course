212da1c5a8c4e87fd10d7848bb66c93a4174dc87
TimzOwen <timzowen@gmail.com>
2022-09-25 21:16:25 +0300

    .

--- Java Practice Questions/Beginner Track/leetcode.java
+++ Java Practice Questions/Beginner Track/leetcode.java
@@ -41,7 +41,31 @@ public class leetcode {
 
         bufferedReader.close();
 
-        
+
+
+        // 4. loops 
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            int sum = a + b;
+            int k = 2;
+            
+            System.out.print(sum+" ");
+            for(int j=0;j<n-1;j++){
+                sum= sum + b*k;
+                k=k*2;
+                System.out.print(sum+" ");
+            }
+            System.out.print("\n");
+            }
+        in.close();
+
+
+        // 5. Application context - Spring 
     }
 
 }
